54b492087b20247888daa3151d275f592f4d87aa
Mohit Mamoria <mohit.mamoria@gmail.com>
2024-08-07 12:06:00 +0530

    added random emojis when any key is pressed

---
+++ resources/js/Pages/Game.vue
@@ -0,0 +1,32 @@
+<script setup>
+import emojis from "@/renderable-emojis.json";
+import { Head } from "@inertiajs/vue3";
+import { onMounted, ref } from "vue";
+
+function renderEmoji(emoji) {
+    const codes = emoji.code.map((code) => "0x" + code);
+    return String.fromCodePoint(...codes);
+}
+
+function getRandomEmoji() {
+    const index = Math.floor(Math.random() * emojis.length);
+
+    return renderEmoji(emojis[index]);
+}
+
+let emoji = ref(getRandomEmoji());
+
+onMounted(() => {
+    addEventListener("keydown", (event) => {
+        emoji.value = getRandomEmoji();
+    });
+});
+</script>
+
+<template>
+    <Head title="👼 Babymoji" />
+
+    <div class="h-dvh flex items-center justify-center">
+        <p class="text-[14rem]">{{ emoji }}</p>
+    </div>
+</template>
